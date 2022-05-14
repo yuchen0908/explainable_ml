7a6b95995952e38244877f6f1315121f42e907cb
yuchen0908 <yuchen0908@gmail.com>
2022-05-15 00:27:38 +1000

    update

--- lib/ml_helper.py
+++ lib/ml_helper.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import learning_curve
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, r2_score
 
 # evaluation
 def calc_metrics(model, X, y):
@@ -65,3 +65,25 @@ def plot_learning_curve(estimator_name, estimator, X, y, ylim=None, cv=None, n_j
     ax.grid(True)
     ax.legend(loc="best")
     return plt
+
+def plot_actual_and_pred(estimator, X, y):
+    """ the function is to plot regression's actual and predicted value
+        so that we can see how predicted values spread across the plane
+    :Args:
+        - estimator, the regressor (from scikit-learn or lightgbm or other libs)
+        - X (numpy.array), the inputs
+        - y (numpy.array), the responses
+    :Return:
+        - plot object for visualisation
+    """
+    f,ax = plt.subplots(figsize=(5,5))
+    ax.scatter(y, estimator.predict(X).reshape(-1,1), color="#2492ff", alpha=0.8, marker=".")
+    ax.plot(y,y, color="#ff9124", alpha=1, linestyle='-.')
+    ax.set_xlim(xmin=0, xmax=8500)
+    ax.set_ylim(ymin=0, ymax=8500)
+    ax.set_xlabel("actual y")
+    ax.set_ylabel("predicted y")
+    ax.set_title(f"actual vs predicted response, R^2 {np.round(r2_score(y, estimator.predict(X).reshape(-1,1)),2)}")
+    ax.grid(True)
+    return plt
+    
